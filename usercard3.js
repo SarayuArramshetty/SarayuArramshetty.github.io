914caf68b936670e06b9799dce33ba7bdd557cf8
SarayuArramshetty <97151471+SarayuArramshetty@users.noreply.github.com>
2023-06-13 22:29:12 +0530

    changes done

--- usercard3.js
+++ usercard3.js
@@ -14,10 +14,10 @@ let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
     .then(data=>{
-        // displayobject.name = data.results[0].name.first + " " + data.results[0].name.last
-        displayobject.name = "User Name"
-        // displayobject.imageurl = data.results[0].picture.large
-        // displayobject.description = data.results[0].gender
+        displayobject.name = data.results[0].name.first + " " + data.results[0].name.last
+        // displayobject.name = "User Name"
+        displayobject.imageurl = data.results[0].picture.large
+        displayobject.description = data.results[0].gender
         document.getElementById("kalam").src = displayobject.imageurl;
         document.getElementById("abdulKalam").innerHTML = displayobject.name;
         document.getElementById("descrip").innerHTML = displayobject.description;
